5eb45a561c7ad7dba848032ec8dc3b07ef9c3537
sujon-chambugong <sujonchambugong@gmail.com>
2022-10-14 10:36:56 +0600

    drowdown profile working with javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('My first');
+
+
+//    profile dropdown 
+    var dropDown = document.getElementById('profile-dropdown');
+    dropDown.style.display = 'none';
+    function profileDrop(){
+
+    if( dropDown && dropDown.style.display == 'block'){
+        dropDown.style.display = 'none';
+    } 
+    else {
+        dropDown.style.display = 'block';
+    }
+}
+
